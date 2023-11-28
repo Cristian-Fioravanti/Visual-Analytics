73db7b7222c2f9406aa4d03e46684e6c1f3cdbda
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2023-11-28 18:22:19 +0100

    aggiunto instogramma di Type (ticks da sistemare)

--- PlayStoreDataApp/fe-new/histogram.js
+++ PlayStoreDataApp/fe-new/histogram.js
@@ -5,12 +5,12 @@ import * as commonService from "./commonService.js"
 // main();
 
 function main() {
-    createHistogramShort(1,dataSet);
-    createHistogramShort(2,dataSet);
-    createHistogramLong(3,dataSet);
+    // createHistogramShort(1,dataSet);
+    // createHistogramShort(2,dataSet);
+    // createHistogramLong(3,dataSet);
 }
   
-export function createHistogramShort(i,dataSet,dataAppName) {
+export function createHistogramShortInstalls(i,dataSet) {
   
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 40},
@@ -27,11 +27,11 @@ var svg = d3.select("#Histogram"+i)
           "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv").then(data=> {
+
 
   // X axis: scale and draw:
   var x = d3.scaleLog()
-      .domain([1, 100000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .domain([1, 100000000000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
       .range([0, width]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -64,106 +64,50 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
         .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
         .attr("height", function(d) { return height - y(d.length); })
         .style("fill", "#69b3a2")
-
-});
-// // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 30, left: 40},
-//     width = 298 - margin.left - margin.right,
-//     height = 165 - margin.top - margin.bottom;
-
-// // append the svg object to the body of the page
-// var svg = d3.select("#Histogram"+i)
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-
-//   // X axis: scale and draw:
-//   var x = d3.scalePoint()
-//       .domain(dataAppName)     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-//       .range([0, width]);
-//   svg.append("g")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(d3.axisBottom(x));
-
-//   // set the parameters for the histogram
-//   var histogram = d3.histogram()
-//       .value(function(d) { return d.Installs; })   // I need to give the vector of value
-//       .domain(x.domain())  // then the domain of the graphic
-
-//   // And apply this function to data to get the bins
-//   var bins = histogram(dataSet);
-
-//   // Y axis: scale and draw:
-//   var y = d3.scaleLinear()
-//       .range([height, 0]);
-//       y.domain([0, d3.max(bins, function(d) { return d.Installs; })]);   // d3.hist has to be called before the Y axis obviously
-//   svg.append("g")
-//       .call(d3.axisLeft(y));
-
-//   // append the bar rectangles to the svg element
-//   svg.selectAll("rect")
-//       .data(bins)
-//       .enter()
-//       .append("rect")
-//         .attr("x", 1)
-//         .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-//         .attr("width", function(d) { return x(d.x1) - x(d.x0) -1; })
-//         .attr("height", function(d) { return height - y(d.length); })
-//         .style("fill", "#69b3a2")
 }
-
-// export function createHistogramLong(i,dataSet) {
+export function createHistogramShortType(i,dataSet,dataAppName,heightY) {
   
-// // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 30, left: 40},
-//     width = 600 - margin.left - margin.right,
-//     height = 165 - margin.top - margin.bottom;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 298 - margin.left - margin.right,
+    height = 165 - margin.top - margin.bottom;
 
-// // append the svg object to the body of the page
-// var svg = d3.select("#Histogram"+i)
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
+// append the svg object to the body of the page
+var svg = d3.select("#Histogram"+i)
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-//   // X axis: scale and draw:
-//   var x = d3.scaleLinear()
-//       .domain([0, d3.max(dataSet, function(d) { return +d.Installs })])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-//       .range([0, width]);
-//   svg.append("g")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(d3.axisBottom(x));
+  // X axis: scale and draw:
+  var x = d3.scalePoint()
+      .domain(dataAppName)     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .attr("class", "HistogramSvg"+i)
+      .call(d3.axisBottom(x).tickSizeInner(10));
+  
+  // And apply this function to data to get the bins
+  // var bins = histogram(dataSet);
 
-//   // set the parameters for the histogram
-//   var histogram = d3.histogram()
-//       .value(function(d) { return d.Installs; })   // I need to give the vector of value
-//       .domain(x.domain())  // then the domain of the graphic
-//       .thresholds(x.ticks(70)); // then the numbers of bins
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+      y.domain([0, heightY+1000]);   // d3.hist has to be called before the Y axis obviously
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-//   // And apply this function to data to get the bins
-//   var bins = histogram(dataSet);
-
-//   // Y axis: scale and draw:
-//   var y = d3.scaleLinear()
-//       .range([height, 0]);
-//       y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-//   svg.append("g")
-//       .call(d3.axisLeft(y));
-
-//   // append the bar rectangles to the svg element
-//   svg.selectAll("rect")
-//       .data(bins)
-//       .enter()
-//       .append("rect")
-//         .attr("x", 1)
-//         .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-//         .attr("width", function(d) { return x(d.x1) - x(d.x0) -1; })
-//         .attr("height", function(d) { return height - y(d.length); })
-//         .style("fill", "#69b3a2")
-
-// }
+  // append the bar rectangles to the svg element
+  svg.selectAll("rect")
+      .data(dataSet)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + (x(d.Type)-5) + "," + y(d.Total) + ")"; })
+        .attr("width", function(d) { return 10 ; })
+        .attr("height", function(d) { return  height- y(d.Total); })
+        .style("fill", "#69b3a2")
+}
--- PlayStoreDataApp/fe-new/pcaSchemas.js
+++ PlayStoreDataApp/fe-new/pcaSchemas.js
@@ -1,10 +1,10 @@
-import * as scatterPlotService  from "./scatterPlot.js";
+import * as scatterPlotService from "./scatterPlot.js";
 import * as ajaxService from "./ajaxService.js";
-import *  as parallelCoordinatesService from "./parallelCoordinates.js";
-import * as categoryService from "./category.js"
-import * as histogramService from "./histogram.js"
+import * as parallelCoordinatesService from "./parallelCoordinates.js";
+import * as categoryService from "./category.js";
+import * as histogramService from "./histogram.js";
 
-import * as commonService from "./commonService.js"
+import * as commonService from "./commonService.js";
 
 main();
 
@@ -13,25 +13,38 @@ function main() {
 }
 
 function getPCAForSchemas() {
-  
   ajaxService.getAllDataPCA().done(function (jsonData) {
-    console.log(jsonData.slice(0,5))
-    commonService.initializeScaleColor(jsonData)
+    console.log(jsonData.slice(0, 5));
+    commonService.initializeScaleColor(jsonData);
     //Creation Schemas
-    categoryService.insertCategory(jsonData)
-    scatterPlotService.createScatterPlot(jsonData)
-    parallelCoordinatesService.createParallelCoordinates(jsonData)
-    //Histogram
-    let dataInstalls = []
-    let dataAppName = []
-    jsonData.forEach(obj => {
-      var objAux = JSON.parse(`{"Installs": ${obj.Installs}}`)
-      dataInstalls.push(objAux)
-      dataAppName.push(obj.App)
-    });
- 
-    histogramService.createHistogramShort(1,dataInstalls, dataAppName)
-    histogramService.createHistogramShort(2,dataInstalls,dataAppName);
+    categoryService.insertCategory(jsonData);
+    scatterPlotService.createScatterPlot(jsonData);
+    parallelCoordinatesService.createParallelCoordinates(jsonData);
+    createHistogramInstalls(jsonData);
     // histogramService.createHistogramLong(3,dataInstall,dataAppName);
+  });
+}
+function createHistogramInstalls(jsonData) {
+  //Histogram
+  let dataInstalls = [];
+  let dataType = [];
+  let dataAppName = [];
+  let dataTypeSet = new Set();
+  jsonData.forEach((obj) => {
+    dataAppName.push(obj.App);
+    var objInstalls = JSON.parse(`{"Installs": ${obj.Installs}}`);
+    dataInstalls.push(objInstalls);
+    
+    dataTypeSet.add(obj.Type)
+  });
+  dataTypeSet.forEach((Type) => {
+  var objType = JSON.parse(`{"Type": "${Type}","Total": ${jsonData.filter(x=> x.Type == Type).length}}`);
+
+  dataType.push(objType)
   })
+
+  let heightY=jsonData.length
+  histogramService.createHistogramShortInstalls(1, dataInstalls);
+  
+  histogramService.createHistogramShortType(2, dataType, Array.from(dataTypeSet),heightY );
 }
