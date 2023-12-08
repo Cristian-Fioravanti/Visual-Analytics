3ec2067d30eb5d423cc2004396ba8971baa10248
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2023-12-08 15:55:23 +0100

    tooltip histo messo

--- PlayStoreDataApp/fe-new/histogram.js
+++ PlayStoreDataApp/fe-new/histogram.js
@@ -119,16 +119,11 @@ export function createHistogramInstalls(dataSet) {
       .style("fill", "#69b3a2");
   }
   popolaTabella([]);
-  popolaTabella(
-    histogram(dataSet.map((obj) => ({ Installs: parseInt(obj.Installs) })))
-  );
+  popolaTabella(histogram(dataSet.map((obj) => ({ Installs: parseInt(obj.Installs) }))));
   commonService.firstSet.observe((data) => {
     if (commonService.mode.value == "Visualize") {
       // And apply this function to data to get the bins
-      dataSet =
-        commonService.firstSet != undefined
-          ? commonService.firstSet.value
-          : allDataInstalls;
+      dataSet = commonService.firstSet != undefined ? commonService.firstSet.value : allDataInstalls;
       var dataY = dataSet.length != 0 ? dataSet : allDataInstalls;
       bins = histogram(dataY);
       // Y axis: scale and draw:
@@ -157,6 +152,66 @@ export function createHistogramInstalls(dataSet) {
 
 export function createHistogramType(dataDistinct, data) {
   var dataSet = data != undefined ? data : [];
+  var domainY = dataSet.length != 0 ? dataSet.length : 7023;
+  var y
+   var tooltip = d3.select("#Histogram1")
+    .append("span")
+    .style("opacity", 0)
+    // .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("position", "absolute")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+
+
+  // A function that change this tooltip when the user hover a point.
+  // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+  }
+
+  var mousemove = function(d) {
+    tooltip
+      .html(y.invert((d3.mouse(this)[1])).toFixed(2))
+      .style("left", (d3.event.clientX+10) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style("top", (d3.event.clientY-50) + "px")
+  }
+
+  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  var mouseleave = function(d) {
+    tooltip.style("opacity", 0)
+    
+  }
+  // var Tooltip;
+  // var rectInvisible
+  // Three function that change the tooltip when user hover / move / leave a cell
+  // var mouseover = function (d) {
+  //   Tooltip.style("opacity", 1);
+  //   var yAxis = svg.select("g.HistogramSvg1");
+  //   if (yAxis.empty()) {
+  //   rectInvisible
+  //     .append("g")
+  //     .call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0))
+  //     .attr("class", "HistogramSvg1");
+  // } 
+  // };
+  // var mousemove = function (d) {
+  //   var mouseX = d3.mouse(this)[0]; // Ottieni la coordinata x del mouse rispetto all'elemento corrente
+  //   var xValue = d3.scaleSymlog().invert(mouseX); // Converte la coordinata x in un valore sull'asse x
+
+  //   // Aggiorna il contenuto della tooltip con il valore sull'asse x
+  //   Tooltip.html("<div>Valore sull'asse x: " + xValue.toFixed(2) + "</div>")
+  //     .style("left", mouseX + "px")
+  //     .style("bottom", "152px");
+  // };
+  // var mouseleave = function (d) {
+  //   Tooltip.style("opacity", 0);
+  // };
+  
+
   function popolaTabella(data) {
     d3.select(".ShortTicks").selectAll("rect").remove();
     d3.select(".ShortTicks")
@@ -185,7 +240,9 @@ export function createHistogramType(dataDistinct, data) {
       .attr("height", function (d) {
         return height - y(d.Total);
       })
-      .style("fill", "#69b3a2");
+      .style("fill", "#69b3a2").on("mouseover", mouseover )
+    .on("mousemove", mousemove )
+    .on("mouseleave", mouseleave );
   }
 
   function updateData(update) {
@@ -201,7 +258,9 @@ export function createHistogramType(dataDistinct, data) {
       .attr("height", function (d) {
         return height - y(d.length);
       })
-      .style("fill", "#69b3a2");
+      .style("fill", "#69b3a2").on("mouseover", mouseover )
+    .on("mousemove", mousemove )
+    .on("mouseleave", mouseleave );
   }
 
   // set the dimensions and margins of the graph
@@ -223,6 +282,7 @@ export function createHistogramType(dataDistinct, data) {
       .classed("ShortTicks", true)
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   }
+  
   // X axis: scale and draw:
   var domainX = dataDistinct.length != 0 ? dataDistinct : ["Free", "Paid"];
   var x = d3
@@ -238,13 +298,10 @@ export function createHistogramType(dataDistinct, data) {
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
   } else {
-    xAxis
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    xAxis.attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
   }
   // Y axis: scale and draw:
-  var domainY = dataSet.length != 0 ? dataSet.length : 7023;
-  var y = d3.scaleSymlog().range([height, 0]).domain([0, domainY]);
+  y = d3.scaleSymlog().range([height, 0]).domain([0, domainY]);
   function generateCustomTicks(y) {
     var tickValues = [y.domain()[0]];
     var currentValue = y.domain()[1];
@@ -264,14 +321,13 @@ export function createHistogramType(dataDistinct, data) {
       .call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0))
       .attr("class", "HistogramSvg1");
   } else {
-    yAxis
-      .call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0))
-      .attr("class", "HistogramSvg1");
+    yAxis.call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0)).attr("class", "HistogramSvg1");
   }
+
+   
+
   popolaTabella([]);
-  const dataType = Array.from(new Set(dataSet.map((obj) => obj.Type))).map(
-    (Type) => ({ Type, Total: dataSet.filter((x) => x.Type === Type).length })
-  );
+  const dataType = Array.from(new Set(dataSet.map((obj) => obj.Type))).map((Type) => ({ Type, Total: dataSet.filter((x) => x.Type === Type).length }));
   popolaTabella(dataType);
 }
 
@@ -296,10 +352,7 @@ export function createHistogramContentRating(dataDistinct, data) {
       .classed("ContentRating", true)
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   }
-  var domainX =
-    dataDistinct.length != 0
-      ? dataDistinct
-      : ["Everyone", "Teen", "Adults Only"];
+  var domainX = dataDistinct.length != 0 ? dataDistinct : ["Everyone", "Teen", "Adults Only"];
   // X axis: scale and draw:
   var x = d3
     .scalePoint()
@@ -314,9 +367,7 @@ export function createHistogramContentRating(dataDistinct, data) {
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
   } else {
-    xAxis
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    xAxis.attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
   }
 
   // Y axis: scale and draw:
@@ -342,9 +393,7 @@ export function createHistogramContentRating(dataDistinct, data) {
       .call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0))
       .attr("class", "HistogramSvg2");
   } else {
-    yAxis
-      .call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0))
-      .attr("class", "HistogramSvg2");
+    yAxis.call(d3.axisLeft(y).tickValues(generateCustomTicks(y)).tickSizeOuter(0)).attr("class", "HistogramSvg2");
   }
 
   function popolaTabella(data) {
@@ -395,9 +444,7 @@ export function createHistogramContentRating(dataDistinct, data) {
       .style("fill", "#69b3a2");
   }
   popolaTabella([]);
-  const dataContent_Rating = Array.from(
-    new Set(dataSet.map((obj) => obj.Content_Rating))
-  ).map((Content_Rating) => ({
+  const dataContent_Rating = Array.from(new Set(dataSet.map((obj) => obj.Content_Rating))).map((Content_Rating) => ({
     Content_Rating,
     Total: dataSet.filter((x) => x.Content_Rating === Content_Rating).length,
   }));
@@ -409,28 +456,14 @@ export function populateHistograms(jsonData) {
   commonService.firstSet.observe((data) => {
     if (commonService.mode.value == "Visualize") {
       var newData = commonService.firstSet.value;
-      createHistogramType(
-        Array.from(new Set(newData.map((obj) => obj.Type))),
-        newData
-      );
-      createHistogramContentRating(
-        Array.from(new Set(newData.map((obj) => obj.Content_Rating))),
-        newData
-      );
+      createHistogramType(Array.from(new Set(newData.map((obj) => obj.Type))), newData);
+      createHistogramContentRating(Array.from(new Set(newData.map((obj) => obj.Content_Rating))), newData);
     }
   });
 
   function initializeHistograms(jsonData) {
-    createHistogramType(
-      Array.from(new Set(jsonData.map((obj) => obj.Type))),
-      jsonData
-    );
-    createHistogramContentRating(
-      Array.from(new Set(jsonData.map((obj) => obj.Content_Rating))),
-      jsonData
-    );
-    createHistogramInstalls(
-      jsonData.map((obj) => ({ Installs: parseInt(obj.Installs) }))
-    );
+    createHistogramType(Array.from(new Set(jsonData.map((obj) => obj.Type))), jsonData);
+    createHistogramContentRating(Array.from(new Set(jsonData.map((obj) => obj.Content_Rating))), jsonData);
+    createHistogramInstalls(jsonData.map((obj) => ({ Installs: parseInt(obj.Installs) })));
   }
 }
