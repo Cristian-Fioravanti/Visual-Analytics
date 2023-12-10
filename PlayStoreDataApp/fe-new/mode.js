a4e26f50ecd28f6dd804de295196fef88ff109f1
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2023-12-10 15:31:46 +0100

    aggiunti tasti zoom e brush in alto + fix bug

--- PlayStoreDataApp/fe-new/mode.js
+++ PlayStoreDataApp/fe-new/mode.js
@@ -1,21 +1,23 @@
 import "https://d3js.org/d3.v5.min.js";
 import "./interface.js";
 import * as commonService from "./commonService.js";
-
+import {setAxisToInitialValue} from "./scatterPlot.js";
 function main() {
   var button = d3.select("#bottone");
   button.on("click", function (d) {
+    setAxisToInitialValue()
     if (button.text() == "Visualize") {
       button.text("Compare");
-      button.classed("button-compare", true)
-      button.classed("button-visualize",false)
+      button.classed("button-compare", true);
+      button.classed("button-visualize", false);
       commonService.setMode("Compare");
+      
     } else {
       button.text("Visualize");
-      button.classed("button-compare", false)
-      button.classed("button-visualize",true)
+      button.classed("button-compare", false);
+      button.classed("button-visualize", true);
       commonService.setMode("Visualize");
-      this.style.backgroundColor="blu"
+      this.style.backgroundColor = "blu";
     }
   });
 }
--- PlayStoreDataApp/fe-new/scatterPlot.js
+++ PlayStoreDataApp/fe-new/scatterPlot.js
@@ -11,15 +11,21 @@ let firstBrush;
 let secondBrush;
 let brushVar;
 let dimensions = ["Rating", "Reviews", "Size", "Installs", "Price", "Content_Rating", "Type"];
-function createScatterPlot(jsonPCAData) {
-  allData = jsonPCAData;
   var x;
   var y;
+  var initilizedX;
+var initilizedY;
+  var newX;
+var newY;
+   var zoom
+function createScatterPlot(jsonPCAData) {
+  allData = jsonPCAData;
+
   var xAxis;
-  var yAxis
-  var newX
-  var newY
-  var tooltip
+  var yAxis;
+  
+  var tooltip;
+
   numberOfBrush = 0;
   secondSet = [];
   firstSet = [];
@@ -28,34 +34,33 @@ function createScatterPlot(jsonPCAData) {
   scaleColor = commonService.getScaleColor();
 
   // set the dimensions and margins of the graph
-  var divWidth = d3.select("#scatterPlot").node().clientWidth;
+  var divWidth = d3.select("#scatterPlot").node().clientWidth + 100;
   var divHeigth = d3.select(".div2").node().clientHeight / 2;
-  var margin = { top: 10, right: 20, bottom: 20, left: 80 },
+  var margin = { top: 10, right: 0, bottom: 20, left: 0 },
     width = divWidth - margin.left - margin.right,
     height = divHeigth - margin.top - margin.bottom;
-  
+
   function addTooltip() {
     // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
-  // Its opacity is set to 0: we don't see it by default.
-  tooltip = d3
-    .select("#scatterPlot")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "black")
-    .style("position", "absolute")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
-
+    // Its opacity is set to 0: we don't see it by default.
+    tooltip = d3
+      .select("#scatterPlot")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "black")
+      .style("position", "absolute")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "10px");
   }
-  addTooltip()
+  addTooltip();
 
   // A function that change this tooltip when the user hover a point.
   // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
   var mouseover = function (d) {
-    tooltip.style("opacity", 1).style("display","block");
+    tooltip.style("opacity", 1).style("display", "block");
   };
 
   var mousemove = function (d) {
@@ -71,15 +76,15 @@ function createScatterPlot(jsonPCAData) {
     });
     tooltip
       .html(htmlContent)
-      .style("left", d3.mouse(this)[0] + 280 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-      .style("top", d3.mouse(this)[1] +70+ "px");
+      .style("left", d3.mouse(this)[0] + 320 + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style("top", d3.mouse(this)[1] + 70 + "px");
   };
 
   // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
   var mouseleave = function (d) {
-    tooltip.style("opacity", 0).style("display","none");
+    tooltip.style("opacity", 0).style("display", "none");
   };
-  
+
   // append the svg object to the body of the page
   var svg = d3.select("#scatterPlot").select("svg").select("g");
   if (svg.empty()) {
@@ -91,13 +96,17 @@ function createScatterPlot(jsonPCAData) {
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   }
-  
+
   // Add zoom functionality
-   var zoom = d3.zoom()
-      .scaleExtent([-20, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
-      .extent([[0, 0], [width, height]])
+  zoom = d3
+    .zoom()
+    .scaleExtent([-20, 20]) // This control how much you can unzoom (x0.5) and zoom (x20)
+    .extent([
+      [0, 0],
+      [width, height],
+    ])
     .on("zoom", zoomed);
-  
+
   // svg.append("rect")
   //     .attr("class","zoomRect")
   //     .attr("width", width)
@@ -111,7 +120,7 @@ function createScatterPlot(jsonPCAData) {
     // recover the new scale
     newX = d3.event.transform.rescaleX(x);
     newY = d3.event.transform.rescaleY(y);
-    
+
     // update axes with these new boundaries
     xAxis.call(d3.axisBottom(newX));
     yAxis.call(d3.axisLeft(newY));
@@ -120,111 +129,112 @@ function createScatterPlot(jsonPCAData) {
     svg
       .selectAll("circle")
       .attr("cx", function (d) {
-      var cx = newX(d.Y1);
-      return cx >= 0 && cx <= width ? cx : cx < 0 ? 0 : width;
-    })
-    .attr("cy", function (d) {
-      var cy = newY(d.Y2);
-      return cy >= 0 && cy <= height ? cy : cy < 0 ? 0 : height;
-    });
+        var cx = newX(d.Y1);
+        return cx >= 0 && cx <= width ? cx : cx < 0 ? 0 : width;
+      })
+      .attr("cy", function (d) {
+        var cy = newY(d.Y2);
+        return cy >= 0 && cy <= height ? cy : cy < 0 ? 0 : height;
+      });
   }
 
-  brushVar =
-    d3
-      .brush() // Add the brush feature using the d3.brush function
-      .extent([
-        [0, 0],
-        [width, height],
-      ]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-      .on("start brush", updateChart)
-      .on("end", createRect); // Each time the brush selection changes, trigger the 'updateChart' function
-    
-  svg.append("g")
-      .attr("class","brushRect")
-      .call(brushVar);
-  
-// Inizializza le variabili di stato per lo zoom e il pennello
-var zoomEnabled = false;
-var brushEnabled = true;
-function updateButtonStyles() {
-  // Pulsante Zoom
+  brushVar = d3
+    .brush() // Add the brush feature using the d3.brush function
+    .extent([
+      [0, 0],
+      [width, height],
+    ]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+    .on("start brush", updateChart)
+    .on("end", createRect); // Each time the brush selection changes, trigger the 'updateChart' function
+
+  svg.append("g").attr("class", "brushRect").call(brushVar);
+
+  // Inizializza le variabili di stato per lo zoom e il pennello
+  var zoomEnabled = false;
+  var brushEnabled = true;
+  function updateButtonStyles() {
+    // Pulsante Zoom
+    var buttonZoom = d3.select("#toggleZoom");
+    buttonZoom.classed("active", zoomEnabled);
+    buttonZoom.classed("disabled", !zoomEnabled);
+
+    // Pulsante Pennello
+    var buttonBrush = d3.select("#toggleBrush");
+    buttonBrush.classed("active", brushEnabled);
+    buttonBrush.classed("disabled", !brushEnabled);
+  }
+  function moveLastChildToFirst(parentElement) {
+    if (parentElement) {
+      var lastChild = parentElement.lastElementChild;
+
+      // Sposta l'ultimo figlio all'inizio
+      parentElement.insertBefore(lastChild, parentElement.firstElementChild);
+    }
+  }
+  updateButtonStyles();
+  // Inizializza il pulsante per attivare/disattivare lo zoom
   var buttonZoom = d3.select("#toggleZoom");
-  buttonZoom.classed("active", zoomEnabled);
-  buttonZoom.classed("disabled", !zoomEnabled);
-
-  // Pulsante Pennello
-  var buttonBrush = d3.select("#toggleBrush");
-  buttonBrush.classed("active", brushEnabled);
-  buttonBrush.classed("disabled", !brushEnabled);
-  }
-function moveLastChildToFirst(parentElement) {
-
-  if (parentElement) {
-    var lastChild = parentElement.lastElementChild;
-
-    // Sposta l'ultimo figlio all'inizio
-    parentElement.insertBefore(lastChild, parentElement.firstElementChild);
-  }
-}
-  updateButtonStyles()
-// Inizializza il pulsante per attivare/disattivare lo zoom
-var buttonZoom = d3.select("#toggleZoom");
-buttonZoom.on("click", function (d) {
-  if (zoomEnabled) {
-    // Disattiva lo zoom
-    d3.select(".zoomRect").remove();
-    x = newX;
-    y = newY;
-  } else {
-    // Verifica se il pennello è attivo e disattivalo
-    if (brushEnabled) {
-      d3.select(".brushRect").remove();
-      brushEnabled = false;
-    }
-
-    // Attiva lo zoom
-    if (d3.select(".zoomRect").empty()) {
-      svg.append("rect")
-        .attr("class", "zoomRect")
-        .attr("width", width)
-        .attr("height", height)
-        .style("fill", "none")
-        .style("pointer-events", "all")
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-        .call(zoom);
-      moveLastChildToFirst(svg._groups[0][0])
-    }
-  }
-  zoomEnabled = !zoomEnabled; // Inverti lo stato dello zoom
-  updateButtonStyles(); 
-});
-
-// Inizializza il pulsante per attivare/disattivare il pennello
-var buttonBrush = d3.select("#toggleBrush");
-buttonBrush.on("click", function (d) {
-  if (brushEnabled) {
-    // Disattiva il pennello
+  buttonZoom.on("click", function (d) {
     d3.select(".brushRect").remove();
-  } else {
-    // Verifica se lo zoom è attivo e disattivalo
+    if (commonService.mode.value == "Visualize") resetToInitialPointVisualize(svg);
+    else putAllEmpty(svg);
+    updateChart();
     if (zoomEnabled) {
+      // Disattiva lo zoom
+      d3.select(".brushRect").remove();
       d3.select(".zoomRect").remove();
-      zoomEnabled = false;
-    }
-
-    // Attiva il pennello
-    if (newX != undefined && newY != undefined) {
       x = newX;
       y = newY;
+    } else {
+      // Verifica se il pennello è attivo e disattivalo
+      if (brushEnabled) {
+        d3.select(".brushRect").remove();
+        brushEnabled = false;
+      }
+
+      // Attiva lo zoom
+      if (d3.select(".zoomRect").empty()) {
+        svg
+          .append("rect")
+          .attr("class", "zoomRect")
+          .attr("width", width)
+          .attr("height", height)
+          .style("fill", "none")
+          .style("pointer-events", "all")
+          .style("cursor", "grab")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+          .call(zoom);
+        moveLastChildToFirst(svg._groups[0][0]);
+      }
     }
-    svg.append("g")
-      .attr("class", "brushRect")
-      .call(brushVar);
-    moveLastChildToFirst(svg._groups[0][0])
-  }
-  brushEnabled = !brushEnabled; // Inverti lo stato del pennello
-    updateButtonStyles(); 
-});
+    zoomEnabled = !zoomEnabled; // Inverti lo stato dello zoom
+    updateButtonStyles();
+  });
+
+  // Inizializza il pulsante per attivare/disattivare il pennello
+  var buttonBrush = d3.select("#toggleBrush");
+  buttonBrush.on("click", function (d) {
+    if (brushEnabled) {
+      // Disattiva il pennello
+      d3.select(".brushRect").remove();
+    } else {
+      // Verifica se lo zoom è attivo e disattivalo
+      if (zoomEnabled) {
+        d3.select(".zoomRect").remove();
+        zoomEnabled = false;
+      }
+
+      // Attiva il pennello
+      if (newX != undefined && newY != undefined) {
+        x = newX;
+        y = newY;
+      }
+      svg.append("g").attr("class", "brushRect").call(brushVar);
+      moveLastChildToFirst(svg._groups[0][0]);
+    }
+    brushEnabled = !brushEnabled; // Inverti lo stato del pennello
+    updateButtonStyles();
+  });
   // Create scales with log transformation for x and y axes
   x = d3
     .scaleLog()
@@ -235,7 +245,7 @@ buttonBrush.on("click", function (d) {
       }) + 10,
     ])
     .range([0, width]);
-
+  initilizedX = x;
   xAxis = svg.select("g.x.axis");
   if (xAxis.empty()) {
     svg
@@ -271,7 +281,7 @@ buttonBrush.on("click", function (d) {
       }) + 10,
     ])
     .range([height, 0]);
-
+  initilizedY = y;
   yAxis = svg.select("g.y.axis");
   if (yAxis.empty()) {
     svg
@@ -356,18 +366,7 @@ buttonBrush.on("click", function (d) {
         if (extent[0][0] == extent[1][0] && extent[0][1] == extent[1][1]) {
           svg.selectAll(".selectedScatterPlot").classed("selectedScatterPlot", true);
           if (numberOfBrush == 2 || (numberOfBrush == 1 && categoryService.numberOfCheckBoxSelected == 1) || categoryService.numberOfCheckBoxSelected == 2) {
-            svg.selectAll(".brush").remove(); // Rimuovi eventuali rettangoli di selezione precedenti
-            svg.selectAll(".selectedScatterPlot").classed("selectedScatterPlot", false);
-
-            numberOfBrush = 0;
-            commonService.setFirstSet([]);
-            commonService.setSecondSet([]);
-            firstSet = [];
-            secondSet = [];
-            firstBrush = [];
-            secondBrush = [];
-            commonService.resetCheckBox();
-            categoryService.resetCategory();
+            putAllEmpty(svg);
           }
           return;
         }
@@ -408,18 +407,7 @@ buttonBrush.on("click", function (d) {
 
         if (numberOfBrush > 1 || (numberOfBrush == 1 && categoryService.numberOfCheckBoxSelected == 1)) {
           console.log("qui");
-          svg.selectAll(".brush").remove(); // Rimuovi eventuali rettangoli di selezione precedenti
-          svg.selectAll(".selectedScatterPlot").classed("selectedScatterPlot", false);
-
-          numberOfBrush = 0;
-          commonService.setFirstSet([]);
-          commonService.setSecondSet([]);
-          firstSet = [];
-          secondSet = [];
-          firstBrush = [];
-          secondBrush = [];
-          commonService.resetCheckBox();
-          categoryService.resetCategory();
+          putAllEmpty(svg);
         }
       }
     }
@@ -520,6 +508,40 @@ function isFirstBrush() {
     (numberOfBrush == 0 && categoryService.numberOfCheckBoxSelected == 1 && commonService.isEmpty(commonService.firstSet)) ||
     (numberOfBrush == 1 && categoryService.numberOfCheckBoxSelected == 0 && firstBrush.length == 0)
   );
+}
+
+function putAllEmpty(svg) {
+  svg.selectAll(".brush").remove(); // Rimuovi eventuali rettangoli di selezione precedenti
+  svg.selectAll(".selectedScatterPlot").classed("selectedScatterPlot", false);
+
+  numberOfBrush = 0;
+  commonService.setFirstSet([]);
+  commonService.setSecondSet([]);
+  firstSet = [];
+  secondSet = [];
+  firstBrush = [];
+  secondBrush = [];
+  commonService.resetCheckBox();
+  categoryService.resetCategory();
+}
+
+function resetToInitialPointVisualize(svg) {
+  svg.selectAll(".brush").remove(); // Rimuovi eventuali rettangoli di selezione precedenti
+  svg.selectAll("circle").classed("selectedScatterPlot", true).classed("selectedScatterPlotFilteredParallel", false);
+  numberOfBrush = 0;
+  commonService.setFirstSet(allData);
+  firstSet = allData;
+  firstBrush = [];
+  commonService.resetCheckBox();
+  categoryService.resetCategory();
+}
+export function setAxisToInitialValue() {
+  if (initilizedX != undefined && initilizedY != undefined) {
+    x = newX;
+    y = newY;
+    
+  }
+  
 }
 
 export { createScatterPlot };
